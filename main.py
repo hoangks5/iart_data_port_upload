47e58047bbbc7d402b679711e21e37888ce396be
hoangks5 <hoangnd.ute@gmail.com>
2024-08-12 18:47:25 +0700

    feat: update schema

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI, File, UploadFile
 import uvicorn
+import os
 
+# list file in directory schema
+reports = os.listdir("schema")
+reports = [report.split(".")[0] for report in reports]
 
 app = FastAPI()
 
@@ -12,7 +16,17 @@ def health():
 # api endpoint post method to send the file data excel
 @app.post("/uploadfile/")
 async def uploadfile(file: UploadFile = File(...)):
-    return {"filename": file.filename}
+    type_report = None
+    for report in reports:
+        if report.lower() in file.filename.lower():
+            type_report = report
+            break
+    if type_report is None:
+        return {"message": "Không tìm thấy loại báo cáo phù hợp. Vui lòng kiểm tra lại tên file thuộc 1 trong các loại sau: " + ", ".join(reports)}
+    account_name = file.filename.split("-")[1].split(".")[0].strip()
+    
+    return {"filename": file.filename, "type_report": type_report, "account_name": account_name}
+    
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=80)
