0c1a2dbe78f4af1c0c0a714d12af7e0897977b3f
hoangks5 <hoangnd.ute@gmail.com>
2024-08-14 09:59:44 +0700

    feat: change port

--- main.py
+++ main.py
@@ -111,5 +111,5 @@ async def uploadfile(file: UploadFile = File(...), region: str = None):
     
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
     
