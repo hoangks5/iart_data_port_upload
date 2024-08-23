8638ea2904e729ae2bd84bb0e02bf77da32519b9
hoangks5 <hoangnd.ute@gmail.com>
2024-08-23 08:23:11 +0700

    feat: skip upfile

--- main.py
+++ main.py
@@ -321,7 +321,10 @@ async def uploadfile(file: UploadFile = File(...), team: str = Form('awe'), plat
         df.to_csv('./archive/' + file.filename, index=False, encoding='utf-8')
       
       
+        
         status = patch_file(f"https://onelake.dfs.fabric.microsoft.com/b8fa8dd8-6181-4d0a-a756-1cc3d08f1244/de223b30-d0a3-469c-94d6-cf7137fcae33/Files/iart/{team}/{platform}/{account_name}/{region}/{time.time()} - {file.filename}", './archive/' + file.filename, f'{time.time()} - {file.filename}')
+        status = True
+        
         os.remove('./archive/' + file.filename)
         if status != True:
             return status
